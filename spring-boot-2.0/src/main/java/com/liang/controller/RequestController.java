c6a4425fdac000b0fdf875f531b6c14cb22d0849
LvanLiang <liangxp0@126.com>
2021-03-08 22:21:25 +0800

    feat:复杂参数、ServletAPI参数的解析封装

--- spring-boot-2.0/src/main/java/com/liang/controller/RequestController.java
+++ spring-boot-2.0/src/main/java/com/liang/controller/RequestController.java
@@ -1,11 +1,14 @@
 package com.liang.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,12 +28,28 @@ public class RequestController {
 
     @GetMapping("/success")
     @ResponseBody
-    public Map success(@RequestAttribute("code") Integer code,
-                       @RequestAttribute("message") String message) {
+    public Map success(@RequestAttribute(value = "code", required = false) Integer code,
+                       @RequestAttribute(value = "message", required = false) String message,
+                       HttpServletRequest request) {
         Map<String, Object> map = new HashMap<>(4);
         map.put("code", code);
         map.put("message", message);
+        // 转发request的参数
+        map.put("requestMap", request.getAttribute("map"));
+        map.put("requestModel", request.getAttribute("model"));
+        map.put("requestMsg", request.getAttribute("requestMsg"));
         return map;
+    }
+
+    @GetMapping("/param")
+    public String getParam(Map<String, Object> map, Model model,
+                           HttpServletRequest request, HttpServletResponse response) {
+        map.put("map", "hello");
+        model.addAttribute("model", "world");
+        request.setAttribute("requestMsg", "hi!");
+        Cookie cookie = new Cookie("key", "258");
+        response.addCookie(cookie);
+        return "forward:/success";
     }
 
 }
